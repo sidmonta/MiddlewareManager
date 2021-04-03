d8c2ba1d69531cb30718525ef59e2076b71a227c
sidmonta <sidmonta.15@gmail.com>
2021-04-03 16:11:14 +0200

    first implementation of tools

--- test.spec.js
+++ test.spec.js
@@ -1,29 +1,34 @@
-import * as P from './lib/index'
+import * as P from "./lib/index"
 
 const deps = {
   logger: {
     info: console.log,
     warn: console.warn,
-    error: console.error
-  }
+    error: console.error,
+  },
 }
 
 const pipe = P.pipe(deps)
 
-const testMiddleware: Middleware<typeof deps, number, number> = ({ logger }) => (num) => {
+const testMiddleware: Middleware<typeof deps, number, number> = ({
+  logger,
+}) => (num) => {
   const i = num * 10
   logger.info(i)
   return i
 }
 
-const testMiddleware2: Middleware<typeof deps, string, number> = ({ logger }) => (num) => {
-  const i = num + 'maionese'
+const testMiddleware2: Middleware<typeof deps, string, number> = ({
+  logger,
+}) => (num) => {
+  const i = num + "maionese"
   logger.info(i)
   return 1000
 }
 
 async function run() {
   const res = await pipe(10, testMiddleware, testMiddleware2)
+  return res
 }
 
-run().then().catch()
+run().then(console.log).catch(console.error)
